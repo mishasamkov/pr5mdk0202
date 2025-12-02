85415f07d7f2f222eebb975bb96aae4c18dc5465
student-a502 <student-a502@STUD-RM5-A502>
2025-12-02 14:49:30 +0500

    добавлены начальные перменные самков

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,10 @@ namespace Client
 {
     internal class Program
     {
+        static IPAddress ServerIpAddress;
+        static int ServerPort;
+        static string ClientTolen;
+        static DateTime ClientDateConnection;
         static void Main(string[] args)
 
         {
